e26574b6ea1c1c1f1191e2c59cd516cb14014e49
John Ko <git@johnko.ca>
2015-01-03 21:54:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
-//var customUploadDir = "/tmp/my/path";
-var subfolders = [
+moduel.exports = {
+//customUploadDir: "/tmp/my/path",
+subfolders: [
 
 'alice',
 'bob',
 'photo',
 'video'  //Last item has no trailing comma
 
-];
+]
+};
